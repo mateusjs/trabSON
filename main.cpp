475d377cdb610d7bf163e03cc435c9dee705c53d
Mateus José <mateusjose19@hotmail.com>
2018-05-22 19:41:48 -0300

    Update main.cpp
    Trabalho finalizado com sucesso!!!

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <semaphore.h>
 #include <unistd.h>
 #include <list>
+#include <time.h>
 
 typedef std::vector<int> Buffer;
 
@@ -13,8 +14,12 @@ using namespace std;
 
 sem_t cozinha;
 sem_t buffet;
-int countCozinha;
-int countBuffet;
+sem_t fullCozinha;
+sem_t fullBuffet;
+sem_t emptyCozinha;
+sem_t emptyBuffet;
+
+
 
 class Cozinha{ //buffer
 public:
@@ -27,6 +32,7 @@ public:
     int tamBufferCozinha;
     int in;
     int out;
+    int countCozinha;
 //    volatile int countCozinha;
 };
 
@@ -40,14 +46,14 @@ public:
     int tamBufferBuffet;
     int in;
     int out;
-//    int countBuffet;
+    int countBuffet;
 };
 
 class Cozinheiro{ //Produtor do buffer cozinha
 public:
     Cozinheiro(){};
-    Cozinha a;
-    Cozinheiro(Cozinha a);
+    Cozinha *a;
+    Cozinheiro(Cozinha *a);
     static void * cozinhando(void *pVoid);
     void * testing(void *pVoid);
 };
@@ -55,8 +61,8 @@ public:
 class Clientes{ //Consumidor do buffer buffet
 public:
     Clientes(){};
-    Buffet a;
-    Clientes(Buffet a);
+    Buffet *a;
+    Clientes(Buffet *a);
     static void * servindo(void *pVoid);
     void * chamaServindo(void *pVoid);
 };
@@ -64,9 +70,9 @@ public:
 class Garcons{ //Consumidor do buffer cozinha e produtor do buffer buffet
 public:
     Garcons(){};
-    Cozinha a;
-    Buffet b;
-    Garcons(Cozinha a, Buffet b);
+    Cozinha *a;
+    Buffet *b;
+    Garcons(Cozinha *a, Buffet *b);
     static void *repondo( void *pVoid);
     void *teste(void *pVoid);
 };
@@ -82,32 +88,42 @@ Cozinha::Cozinha(int tamBufferCozinha) {
     in = 0;
     out = 0;
     bufferCozinha = Buffer(tamBufferCozinha);
+	for(int i = 0; i < tamBufferCozinha; i++)
+		bufferCozinha[i]=0;
 }
 
 void Cozinha::produzCozinha() {
-    while(countCozinha == tamBufferCozinha);
-
-    bufferCozinha[in] = 1;
-    in = (in + 1) % tamBufferCozinha;
-    int sem_wait(sem_t &cozinha);
-    countCozinha++;
-    cout<< "Poduziu Cozinha" << countCozinha << this << endl;
+   
+	int sem_wait(sem_t &emptyCozinha);
+	int sem_wait(sem_t &cozinha);
+		//cout << "\nproduz em " << in << endl;
+		//for(int i = 0; i < tamBufferCozinha; i++){
+			//cout<<" "<< bufferCozinha[i];
+		//}
+		//cout << endl;
+		sleep(3);
+		bufferCozinha[in] = 1;
+		in = (in + 1) % tamBufferCozinha;
     int sem_post(sem_t &cozinha);
+	int sem_post(sem_t &fullCozinha);
 }
 
 void Cozinha::consomeCozinha() {
     int retira_bandeja = 0;
-    while(countCozinha == 0) {
-        cout << countCozinha <<  this;  sleep(1);
-        fflush(stdout);
-    }
-
-    retira_bandeja = bufferCozinha[out];
-    out = (out + 1) % tamBufferCozinha;
+    int sem_wait(sem_t &fullCozinha);
     int sem_wait(sem_t &cozinha);
-    countCozinha--;
-    cout<< "Consumiu Cozinha" << endl;
-    int sem_post(sem_t &cozinha);
+
+		//cout << "\nconsome em " << out << endl;
+		//for(int i = 0; i < tamBufferCozinha; i++){
+			//cout<<" "<< bufferCozinha[i];
+		//}
+		//cout << endl;
+		sleep(1);
+		retira_bandeja = bufferCozinha[out];
+		out = (out + 1) % tamBufferCozinha;
+
+    int sem_post(sem_t &cozinha);	
+    int sem_post(sem_t &emptyCozinha);
 
 }
 
@@ -123,32 +139,32 @@ Buffet::Buffet(int tamBufferBuffet) {
 }
 
 void Buffet::produzBuffet() {
-    while(countBuffet == tamBufferBuffet);
+	int sem_wait(sem_t &emptyBuffet);
+	int sem_wait(sem_t &buffet);
+		sleep(1);	
+		bufferBuffet[in] = 1;
+		in = (in + 1) % tamBufferBuffet;
 
-    bufferBuffet[in] = 1;
-    in = (in + 1) % tamBufferBuffet;
-    int sem_wait(sem_t &buffet);
-    countBuffet++;
-    cout<< "Produziu Buffet" << endl;
     int sem_post(sem_t &buffet);
+	int sem_post(sem_t &fullBuffet);
 }
 
 void  Buffet::consomeBuffet() {
     int retira_bandeja = 0;
-    while(countBuffet == 0);
 
-    retira_bandeja = bufferBuffet[out];
-    out = (out + 1) % tamBufferBuffet;
+    int sem_wait(sem_t &fullBuffet);
     int sem_wait(sem_t &buffet);
-    countBuffet--;
-    cout<< "Consumiu Buffet" << endl;
-    int sem_post(sem_t &buffet);
+		sleep(3);
+		retira_bandeja = bufferBuffet[out];
+		out = (out + 1) % tamBufferBuffet;
+    int sem_post(sem_t &buffet);	
+    int sem_post(sem_t &emptyBuffet);
 
 }
 
 // ------------------------------------- METODOS DO CLASSE COZINHEIROS-----------------------------
 
-Cozinheiro::Cozinheiro(Cozinha a) {
+Cozinheiro::Cozinheiro(Cozinha *a) {
     this->a = a;
 }
 
@@ -157,15 +173,15 @@ void * Cozinheiro::cozinhando(void *pVoid) {
 }
 
 void * Cozinheiro::testing(void *pVoid){
-    for(int i = 0 ; i < 10 ; i++){
-        a.produzCozinha();
-        cout << "Cozinheiro cozinhou" << endl;
+    for(int i = 0 ; i < 4; i++){
+        a->produzCozinha();
+        //cout << "\n" << "Cozinheiro cozinhou" << endl;
     }
 }
 
 // ------------------------------------- METODOS DO CLASSE CLIENTES-----------------------------
 
-Clientes::Clientes(Buffet a){
+Clientes::Clientes(Buffet *a){
     this->a = a;
 }
 
@@ -174,15 +190,15 @@ void * Clientes::servindo(void *pVoid){
 }
 
 void * Clientes::chamaServindo(void *pVoid){
-    for(int i = 0 ; i < 10 ; i++){
-        a.consomeBuffet();
-        cout << "Cliente se serviu!" << endl;
+    for(int i = 0 ; i < 4 ; i++){
+        a->consomeBuffet();
+        //cout << "Cliente se serviu!" << endl;
     }
 }
 
 // ------------------------------------- METODOS DO CLASSE GARCONS-----------------------------
 
-Garcons::Garcons(Cozinha a, Buffet b){
+Garcons::Garcons(Cozinha *a, Buffet *b){
     this->a = a;
     this->b = b;
 }
@@ -192,49 +208,92 @@ void * Garcons::repondo(void * pVoid){
 }
 
 void * Garcons::teste(void *pVoid){
-    for(int i = 0 ; i < 10; i++){
-        a.consomeCozinha();
-        cout << "Garcon pegou da cozinha" << endl;
-        b.produzBuffet();
-        cout << "Garcon repos no buffet" << endl;
+    for(int i = 0 ; i < 4; i++){
+        a->consomeCozinha();
+        //cout << "Garcon pegou da cozinha" << endl;
+        b->produzBuffet();
+        //cout << "Garcon repos no buffet" << endl;
     }
 }
 
 // ------------------------------------- Fim dos metodos-----------------------------
+	// Coversão de nanosegundo para segundo
+
+
+
+double timespecToSec(struct timespec *time){
+   	return (double)(time->tv_sec * 1000000000 + time->tv_nsec)/1000000000.0;
+}
+
 int main() {
     vector<Cozinheiro> chefs;
     vector<Garcons> campeoes;
     vector<Clientes> donoDaRazao;
 
-    int tam_bufferCozinha = 5;
-    int tam_bufferBuffet = 5;
+    int tam_bufferCozinha = 1;
+    int tam_bufferBuffet = 1;
+	float tempo	= 0.0;
 
-    Cozinha a = Cozinha(tam_bufferCozinha);
-    Buffet b = Buffet(tam_bufferBuffet);
+	struct timespec startCPU, endCPU;
+	struct timespec startWALL, endWALL;
+	double timeCPU, timeWALL, mbs;
 
-    pthread_t cozinheiros[3], garcons[3], clientes[3]; //nome das threads
+	clock_gettime(CLOCK_MONOTONIC,&startWALL); // relógio de "parede"
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&startCPU); // relógio de CPU
 
-    sem_init(&cozinha, 0, tam_bufferCozinha); //iniciaçao das threads
-    sem_init(&buffet, 0, tam_bufferBuffet);
+    Cozinha a(tam_bufferCozinha);
+    Buffet b(tam_bufferBuffet);
+
+    pthread_t cozinheiros[50], garcons[50], clientes[50]; //nome das threads
+
+    sem_init(&cozinha, 0, 1); //semaforos
+    sem_init(&buffet, 0, 1);
+
+    sem_init(&fullCozinha, 0, 0); //semaforos
+    sem_init(&fullBuffet, 0, 0);
+
+    sem_init(&emptyCozinha, 0, tam_bufferCozinha); //semaforos
+    sem_init(&emptyBuffet, 0, tam_bufferBuffet);
 
 
-    for(int i = 0; i < 10; i++) { //insere o buffer no vetor
-        chefs.push_back(Cozinheiro(a));
-        campeoes.push_back(Garcons(a,b));
-        donoDaRazao.push_back(Clientes(b));
+
+    for(int i = 0; i < 50; i++) { //insere o buffer no vetor
+        chefs.push_back(Cozinheiro(&a));
+        campeoes.push_back(Garcons(&a,&b));
+        donoDaRazao.push_back(Clientes(&b));
     }
 
-    for(int i = 0; i < 2 ; i++){ //cria as threadas nos metodos escolhidos
+	clock_gettime(CLOCK_MONOTONIC,&startWALL); // relógio de "parede"
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&startCPU); // relógio de CPU
+
+    for(int i = 0; i < 25 ; i++) //cria as threadas nos metodos escolhidos
         pthread_create(&cozinheiros[i], nullptr, &Cozinheiro::cozinhando, &chefs[i]);
-        pthread_create(&garcons[i], nullptr, &Garcons::repondo, &campeoes[i]);
-        pthread_create(&clientes[i], nullptr, &Clientes::servindo, &donoDaRazao[i]);
+ 
+	for(int i = 0; i < 25 ; i++)
+    	pthread_create(&garcons[i], nullptr, &Garcons::repondo, &campeoes[i]);
 
-    }
+	for(int i = 0; i < 25 ; i++)
+		pthread_create(&clientes[i], nullptr, &Clientes::servindo, &donoDaRazao[i]);
 
-    for(int i=0 ;i < 2;i++) { // espera as threads terminarem a execuçao
+    //JOINS --------------------------------------------------------------------------------
+
+    for(int i=0 ; i < 25;i++) // espera as threads terminarem a execuçao
         pthread_join(cozinheiros[i], nullptr);
+
+    for(int i = 0; i < 25; i++)
         pthread_join(garcons[i], nullptr);
+
+    for(int i = 0; i < 25; i++)
         pthread_join(clientes[i], nullptr);
-    }
+    
+
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&endCPU); // relógio de CPU
+    clock_gettime(CLOCK_MONOTONIC,&endWALL); // relógio de "parede"
+
+    timeCPU  = timespecToSec(&endCPU)  - timespecToSec(&startCPU) ;
+    timeWALL = timespecToSec(&endWALL) - timespecToSec(&startWALL);
+
+	cout<< timeCPU <<", "<< timeWALL<<endl;
+	
     return 0;
 }
